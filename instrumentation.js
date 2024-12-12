77ca384760056e1b95ee4af1c9cfc4610a2dcf17
Tony Powell <apowell@arize.com>
2024-12-12 11:55:57 -0500

    openinference 2.0 fixes esm

--- instrumentation.js
+++ instrumentation.js
@@ -4,12 +4,37 @@ import {
   isPatched,
 } from "@arizeai/openinference-instrumentation-openai";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import * as OpenAI from "openai";
+import { Resource } from "@opentelemetry/resources";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 export { isPatched };
 
-const provider = new NodeTracerProvider();
+const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: "openai-service",
+    [SEMRESATTRS_PROJECT_NAME]: "openai-service",
+  }),
+  spanProcessors: [
+    // new SimpleSpanProcessor(new ConsoleSpanExporter()),
+    new SimpleSpanProcessor(
+      new OTLPTraceExporter({
+        url: "http://localhost:6006/v1/traces",
+      })
+    ),
+  ],
+});
+
 provider.register();
 
+// span exporter
+
+const instrumentation = new OpenAIInstrumentation();
+instrumentation.manuallyInstrument(OpenAI);
+
 registerInstrumentations({
-  instrumentations: [new OpenAIInstrumentation()],
+  instrumentations: [instrumentation],
 });
